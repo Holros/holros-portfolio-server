7e11f1ba49e1ac962f0b44b6264343488d449640
Ajayi Olamide <olasco4real28@gmail.com>
2025-07-09 13:20:05 +0100

    build

--- dist/utils/createError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createError = createError;
-function createError(message, statusCode = 400) {
-    const err = new Error(message);
-    err.statusCode = statusCode;
-    return err;
-}
-//# sourceMappingURL=createError.js.map
